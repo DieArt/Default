545e7209a4c4a976a9a6977030c25540512f38c7
Jean <github@dubito.me>
2018-04-03 11:07:46 +0200

    add nano colour

--- default.sh
+++ default.sh
@@ -6,4 +6,9 @@ htop \
 zsh \
 -y
 
+> ~/.nanorc \
+&& for i in `ls /usr/share/nano/ -l \
+| grep -v man-html \
+| awk '{print $9}'`; do echo "\"include /usr/share/nano/$i\"" >> ~/.nanorc ; done && cat ~/.nanorc
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
